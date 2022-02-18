0d699288df360c66c1d225f94a83db9d23cfa4c8
AllenLawrenceBolanos <allen.lawrence.bolanos@codeandtheory.com>
2022-02-18 14:25:47 +0800

    Updated index, styles, and scripts

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -22,23 +22,6 @@ async function init() {
   supportingCards.forEach(card => {
     $supportingCards.innerHTML += renderCard(card);
   });
-
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '679443763092283',
-      xfbml      : true,
-      version    : 'v13.0'
-    });
-    FB.AppEvents.logPageView();
-  };
-  
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "https://connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
 }
 
 function renderCard(card) {
